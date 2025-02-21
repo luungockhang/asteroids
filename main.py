ee92684cc287c123bc2b5a7e5c3a4364fb83430c
Kangaroo6991 <luungockhang@gmail.com>
2025-02-21 16:50:54 +0000

    updated main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # stdlib
 import pygame
 # constants
+from player import Player
 import constants
 from constants import *
 
@@ -10,6 +11,7 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     
     # Game clock and delta time
     clock = pygame.time.Clock()
@@ -20,9 +22,11 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill(pygame.Color(0,0,0))
-        
-        dt = clock.tick(60) / 1000
+        player.draw(screen)
         pygame.display.flip()
-    
+        
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
+            
 if __name__ == "__main__":
     main()
