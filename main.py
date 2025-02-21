b55830b3f924d9da0120e69cc8e7c5979e0598c7
Kangaroo6991 <luungockhang@gmail.com>
2025-02-21 16:17:12 +0000

    added circleshape.py and updated constants.py

--- main.py
+++ main.py
@@ -10,12 +10,18 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+    
+    # Game clock and delta time
+    clock = pygame.time.Clock()
+    dt = 0
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill(pygame.Color(0,0,0))
         
+        dt = clock.tick(60) / 1000
         pygame.display.flip()
     
 if __name__ == "__main__":
